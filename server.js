4080ccd7c56daa9918979ed6fdad05b0c3024ba2
Sarah Strange <strange1120@gmail.com>
2020-09-17 22:59:32 -0400

    pushing up to hit api

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+const connectionString =
+  "mongodb+srv://Sarah:5XExSYViEAdQAGYa@restaurants.yxgpu.gcp.mongodb.net/Restaurant-Data?retryWrites=true&w=majority";
+
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  restaurantCollection
+    .insertOne(req.body)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.error(error));
+});
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
+    const db = client.db("Restaurant-Data");
+    const restaurantCollection = db.collection("Restaurant-Data");
+  }
+);
