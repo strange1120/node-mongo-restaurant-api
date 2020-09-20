b3180f010bff00ad32558d20796578417a06021c
Sarah Strange <strange1120@gmail.com>
2020-09-19 23:39:24 -0400

    testing again

--- server.js
+++ server.js
@@ -6,31 +6,31 @@ const MongoClient = require("mongodb").MongoClient;
 const connectionString =
   "mongodb+srv://Sarah:5XExSYViEAdQAGYa@restaurants.yxgpu.gcp.mongodb.net/Restaurant-Data?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-app.post("/", (req, res) => {
-  restaurantCollection
-    .insertOne(req.body)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => console.error(error));
-});
+// app.post("/", (req, res) => {
+//   restaurantCollection
+//     .insertOne(req.body)
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((error) => console.error(error));
+// });
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
-  (client) => {
-    console.log("Connected to Database");
-    const db = client.db("Restaurant-Data");
-    const restaurantCollection = db.collection("Restaurant-Data");
-  }
-);
+// MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+//   (client) => {
+//     console.log("Connected to Database");
+//     const db = client.db("Restaurant-Data");
+//     const restaurantCollection = db.collection("Restaurant-Data");
+//   }
+// );
+
+app.listen(8080, () => {
+  console.log("listening on 3000");
+});
 
 // testing syncing
